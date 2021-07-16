a6128f2fcf8beb21f70e769ec28f15b9decfc1cb
mahirafsar <70110496+mahirafsar@users.noreply.github.com>
2021-07-16 16:08:07 +0600

    Update pagetwo.php

--- Final_LabTask_4/view/pagetwo.php
+++ Final_LabTask_4/view/pagetwo.php
@@ -17,62 +17,18 @@ header("Location: ../control/login.php"); // Redirecting To Home Page
 <h2>Profile Page</h2>
 
 Hii, <h3><?php echo $_SESSION["username"];?></h3>
-
-<?php
-$radio1=$radio2=$radio3="";
-$firstname=$email=$address="";
-$connection = new db();
-$conobj=$connection->OpenCon();
-
-$userQuery=$connection->CheckUser($conobj,"student",$_SESSION["username"],$_SESSION["password"]);
-$userQuery1=$connection->searchUser($conobj,"student",$_SESSION["username"]);
-if ($userQuery->num_rows > 0) {
-
-    // output data of each row
-    while($row = $userQuery->fetch_assoc()) {
-      $firstname=$row["firstname"];
-      $email=$row["email"];
-     
-      if($row["gender"]=="female" )
-      { $radio1="checked"; }
-      else if($row["gender"]=="male")
-      { $radio2="checked"; }
-      else{$radio3="checked";}
-      
-      $address=$row["address"];
-  } 
-}
-  else {
-    echo "0 results";
-  }
-
-  if ($userQuery1->num_rows > 0) {
-    // output data of each row
-    while($row = $userQuery1->fetch_assoc()) {
-      $firstname=$row["firstname"];
-      $email=$row["email"];
-     
-      if($row["gender"]=="female" )
-      { $radio1="checked"; }
-      else if($row["gender"]=="male")
-      { $radio2="checked"; }
-      else{$radio3="checked";}
-      
-      $address=$row["address"];
-    }
-    } 
-    else {
-    echo "0 results";
-    }
-
-?>
-<form action='' method='post'>
-
-Search by username : <input type='text' name='username' value="" >
-<input name='search' type='submit' value='search'><br>
-
 <br>Your Profile Page.
 <br><br>
+<form action="" method="post">
+
+Search by username : <br>
+<input type='text' name='search_username' value="" >
+<input name='search' type='submit' value='search'> 
+</form>
+
+Update Form: <br><br>
+<form action='' method='post'>
+<input type="hidden" name="username" value="<?php echo $username; ?>">
 firstname : <input type='text' name='firstname' value="<?php echo $firstname; ?>" ><br><br>
 
 email : <input type='text' name='email' value="<?php echo $email; ?>" ><br><br>
@@ -91,10 +47,8 @@ Address: <input type='text' name='address' value="<?php echo $address; ?>" ><br>
 <a href="../view/pageone.php">Back </a>
 <br>
 
-<a href="../control/logout.php"> logout</a><br>
-
+<a href="../control/logout.php"> logout</a>
 
 </body>
 </html>
-
 
