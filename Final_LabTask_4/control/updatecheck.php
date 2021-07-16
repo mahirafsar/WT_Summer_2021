d8bf3dac7b0a4b11e4dad14e0c664b44327359b0
mahirafsar <70110496+mahirafsar@users.noreply.github.com>
2021-07-16 16:10:21 +0600

    Update updatecheck.php

--- Final_LabTask_4/control/updatecheck.php
+++ Final_LabTask_4/control/updatecheck.php
@@ -3,6 +3,8 @@ include('../model/db.php');
 
 
  $error="";
+ $radio1=$radio2=$radio3="";
+$firstname=$email=$address="";
 
 if (isset($_POST['update'])) {
 if (empty($_POST['firstname']) || empty($_POST['email'])) {
@@ -13,7 +15,7 @@ else
 $connection = new db();
 $conobj=$connection->OpenCon();
 
-$userQuery=$connection->UpdateUser($conobj,"student",$_SESSION["username"],$_POST['firstname'],$_POST['email'],$_POST['gender'],$_POST['address']);
+$userQuery=$connection->UpdateUser($conobj,"student",$_POST["username"],$_POST['firstname'],$_POST['email'],$_POST['gender'],$_POST['address']);
 if($userQuery==TRUE)
 {
     echo "update successful"; 
@@ -27,29 +29,35 @@ $connection->CloseCon($conobj);
 }
 }
 
-else if (isset($_POST['search'])) {
-    if (empty($_POST['usernames'])) {
-    $error = "input given is invalid";
-    }
-    else
-    {
-    $connection = new db();
-    $conobj=$connection->OpenCon();
-    
-    $userQuery=$connection->searchUser($conobj,"student",$_SESSION['usernames']);
-    
-    /*if($userQuery==TRUE)
-    {
-        echo "update successful"; 
-    }
-    else
-    {
-        echo "could not update";    
-    }*/
-    $connection->CloseCon($conobj);
-    
-    }
-    }
+if(isset($_POST['search'])){
+    if (empty($_POST['search_username'])) {
+        $errorinfo = "Please provide a username";
+    }else{
+        
+        $connection = new db();
+        $conobj=$connection->OpenCon();
 
+        $userQuery1=$connection->SearchUser($conobj,"student",$_POST["search_username"]);
+
+        if ($userQuery1->num_rows!=null) {
+            if ($userQuery1->num_rows > 0) {
+                while($row = $userQuery1->fetch_assoc()) {
+                $username = $row["username"];
+                $firstname=$row["firstname"];
+                $email=$row["email"];
+                if(  $row["gender"]=="female" )
+                { $radio1="checked"; }
+                else if($row["gender"]=="male")
+                { $radio2="checked"; }
+                else{$radio3="checked";} 
+                $address=$row["address"]; 
+                } 
+            }
+        }else{
+            $errorinfo= "No result found";
+        }
+    
+    }
+}
 
 ?>
